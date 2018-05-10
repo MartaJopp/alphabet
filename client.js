832e5837e1078202bc518549be55e7d6f0364cbf
Marta Jopp <martapeterson@gmail.com>
2018-05-09 23:24:06 -0500

    speech upon key press

--- client.js
+++ client.js
@@ -1,8 +1,15 @@
 console.log('sourced')
 
 
-if ('speechSynthesis' in window) {
-    var speech = new SpeechSynthesisUtterance('The Letter A');
-    speech.lang = 'en-US';
-    window.speechSynthesis.speak(speech);
-}
+// if ('speechSynthesis' in window) {
+//     var speech = new SpeechSynthesisUtterance('The Letter A');
+//     speech.lang = 'en-US';
+//     window.speechSynthesis.speak(speech);
+// }
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 65) {
+        var speech = new SpeechSynthesisUtterance('The Letter A');
+        speech.lang = 'en-US';
+        window.speechSynthesis.speak(speech)    }
+});
