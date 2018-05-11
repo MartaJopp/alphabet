3c1026c7fb4e68f4973601447e053b9356f0a916
Marta Jopp <martapeterson@gmail.com>
2018-05-10 22:14:32 -0500

    changed voice array inlcudes

--- client.js
+++ client.js
@@ -12,11 +12,18 @@ var keyCodesToSpeech = [
 ]
 var ignoreKeyCodeArray = [20, 9, 13, 93, 187, 27]
 
+let voices = [];
+window.speechSynthesis.onvoiceschanged = function () {
+    voices = window.speechSynthesis.getVoices();
+};
 
 document.addEventListener('keydown', function (e) {
-  
-        var speech = new SpeechSynthesisUtterance(e.key);
-        speech.lang = 'en-US';
-        window.speechSynthesis.speak(speech)
-
+ if (ignoreKeyCodeArray.includes(e.keyCode))  {
+return
+} else {
+     var speech = new SpeechSynthesisUtterance(e.key);
+     speech.voice = voices[40];
+     speech.lang = 'en-US';
+     window.speechSynthesis.speak(speech)
+}
 });
